6f7770656722e7a97723e2feabb7a219c7766d9e
ähsan javaid <hsan786cs@gmail.com>
2021-06-01 19:20:25 +0500

    feat (targets endpoints): Add ping-tree endpoints
    Implemented crud operation on /api/targets route and visitor decision api

--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,88 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+test.serial.cb('Post a target', function (t) {
+  const url = '/api/targets'
+  const body = {
+    url: 'http://example1.com',
+    value: '2.50',
+    maxAcceptsPerDay: '5',
+    accept: {
+      geoState: {
+        $in: ['ca', 'ny']
+      },
+      hour: {
+        $in: ['13', '14', '15']
+      }
+    }
+  }
+  const req = servertest(server(), url, { encoding: 'json', method: 'POST' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'statusCode')
+    t.is(typeof res.body, 'object', 'body is object')
+    t.end()
+  })
+  req.end(JSON.stringify(body))
+})
+
+test.serial.cb('Get all targets', function (t) {
+  const url = '/api/targets'
+  servertest(server(), url, { encoding: 'json', method: 'GET' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'statusCode')
+    t.is(res.body.length, 1, 'response has one target')
+    t.end()
+  })
+})
+
+test.serial.cb('Get target by id', function (t) {
+  const url = '/api/targets/1'
+  servertest(server(), url, { encoding: 'json', method: 'GET' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'statusCode')
+    t.is(typeof res.body, 'object', 'body is object')
+    t.end()
+  })
+})
+
+test.serial.cb('Update target by id', function (t) {
+  const url = '/api/targets/1'
+  const body = {
+    url: 'http://updatedUrl.com',
+    value: '2.50',
+    maxAcceptsPerDay: '5',
+    accept: {
+      geoState: {
+        $in: ['ca', 'ny']
+      },
+      hour: {
+        $in: ['13', '14', '15']
+      }
+    }
+  }
+  const req = servertest(server(), url, { encoding: 'json', method: 'POST' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'statusCode')
+    t.is(typeof res.body, 'object', 'body is object')
+    t.is(res.body.url, 'http://updatedUrl.com', 'updated the url')
+    t.end()
+  })
+  req.end(JSON.stringify(body))
+})
+
+test.serial.cb('Visitor route', function (t) {
+  const url = '/route'
+  const body = {
+    geoState: 'ca',
+    publisher: 'abc',
+    timestamp: '2021-06-02T13:32:51.060Z'
+  }
+  const req = servertest(server(), url, { encoding: 'json', method: 'POST' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'statusCode')
+    t.is(typeof res.body, 'object', 'body is object')
+    t.end()
+  })
+  req.end(JSON.stringify(body))
+})
